6a3d87bda70d159d4afb8c4b433f3d74b752537c
kolesnikov-sa <kolesnikov-sa@ya.ru>
2021-03-04 15:14:32 +0300

    Completed Steps 1 and 2 — before the concept of 'State' is implemented. Product list analyzed automatically.

--- src/Components/FilterableProductTable/ProductTable/ProductTable.tsx
+++ src/Components/FilterableProductTable/ProductTable/ProductTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {ReactElement} from "react";
 import s from './ProductTable.module.css';
 import ProductCategoryRow from "./ProductCategoryRow/ProductCategoryRow";
 import ProductRow from "./ProductRow/ProductRow";
@@ -9,20 +9,24 @@ type ProductTableType = {
 }
 
 function ProductTable(props: ProductTableType) {
+    const rows: Array<ReactElement> = [];
+    let lastCategory: string = '';
+    props.products.forEach((product) => {
+        if (product.category !== lastCategory) {
+            rows.push(<ProductCategoryRow category={product.category}/>);
+            rows.push(<ProductRow name={product.name} price={product.price} stocked={product.stocked}/>)
+            lastCategory = product.category;
+        } else {
+            rows.push(<ProductRow name={product.name} price={product.price} stocked={product.stocked}/>)
+        }
+    });
     return (
         <div className={s.ProductTable}>
             <div className={s.ProductTable__Title}>
                 <div className={s.ProductTable__TitleText}>Name</div>
                 <div className={s.ProductTable__TitleText}>Price</div>
             </div>
-            <ProductCategoryRow category={props.products[0].category}/>
-            <ProductRow name={props.products[0].name} price={props.products[0].price} stocked={props.products[0].stocked}/>
-            <ProductRow name={props.products[1].name} price={props.products[1].price} stocked={props.products[1].stocked}/>
-            <ProductRow name={props.products[2].name} price={props.products[2].price} stocked={props.products[2].stocked}/>
-            <ProductCategoryRow category={props.products[3].category}/>
-            <ProductRow name={props.products[3].name} price={props.products[0].price} stocked={props.products[3].stocked}/>
-            <ProductRow name={props.products[4].name} price={props.products[1].price} stocked={props.products[4].stocked}/>
-            <ProductRow name={props.products[5].name} price={props.products[2].price} stocked={props.products[5].stocked}/>
+            {rows}
         </div>
     );
 }
